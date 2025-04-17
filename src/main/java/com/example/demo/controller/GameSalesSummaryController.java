1faf0793caedad9e3d07b5e5d9c7878c08605ac2
jeff <jeffrey_chan20@hotmail.com>
2025-04-18 00:34:26 +0800

    v8

--- src/main/java/com/example/demo/controller/GameSalesSummaryController.java
+++ src/main/java/com/example/demo/controller/GameSalesSummaryController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.List;
 
 @RestController
 @RequestMapping("/getTotalSales")
